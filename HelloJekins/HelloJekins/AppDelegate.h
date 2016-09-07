5ea95850816506d29dea72765fe4b2bafccf6c7b
iamcoderloser <642872728@qq.com>
2016-09-07 19:59:40 +0800

    用于Jekins打包

---
+++ HelloJekins/HelloJekins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloJekins
+//
+//  Created by 唯你 on 16/9/7.
+//  Copyright © 2016年 lyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloJekins/HelloJekins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloJekins
+//
+//  Created by 唯你 on 16/9/7.
+//  Copyright © 2016年 lyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
